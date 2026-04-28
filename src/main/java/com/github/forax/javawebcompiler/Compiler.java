82bb496af4d78291fae55968fdabbc8079e0d7aa
AnjosLu <100096325+AnjosLu@users.noreply.github.com>
2026-04-28 14:17:49 +0200

    Add check for Java compiler availability and throw exception IllegalStateException if missing.
    # Conflicts:
    #	src/main/java/com/github/forax/javawebcompiler/Compiler.java

--- src/main/java/com/github/forax/javawebcompiler/Compiler.java
+++ src/main/java/com/github/forax/javawebcompiler/Compiler.java
@@ -10,46 +10,48 @@ import java.util.List;
 import java.util.Locale;
 
 public class Compiler {
-  private Compiler(){
-      throw new AssertionError("no instances");
-  }
-  record Diagnostic(long line, long column, String message) {}
-  private record CompilerResult(boolean success, DiagnosticCollector<Object> diagnostics, MemoryClassLoader loader) {}
+    record Diagnostic(long line, long column, String message) {}
+    private record CompilerResult(boolean success, DiagnosticCollector<Object> diagnostics) {}
 
-  // Package private for testing
-  static List<Diagnostic> compileInMemory(String className, String sourceCode, MemoryClassLoader loader) {
-    var compileResult = compileTask(className, sourceCode, loader);
-    return compilationResultHandler(compileResult);
-  }
+    // Package private for testing
+    static List<Diagnostic> compileInMemory(String className, String sourceCode) {
+        var compileResult = compileTask(className, sourceCode);
+        return compilationResultHandler(compileResult);
+    }
 
   private static CompilerResult compileTask (String className, String sourceCode, MemoryClassLoader loader){
     var compiler = ToolProvider.getSystemJavaCompiler();
-    var diagnostics = new DiagnosticCollector<>();
-    var fileManager = new MemoryFileManager(compiler.getStandardFileManager(null, null, null), loader);
-
-    var file = new SimpleJavaFileObject(
-            URI.create("string:///" + className.replace('.', '/') + JavaFileObject.Kind.SOURCE.extension),
-            JavaFileObject.Kind.SOURCE) {
-        @Override
-        public CharSequence getCharContent(boolean ignoreEncodingErrors) {
-            return sourceCode;
+    if(compiler == null){
+          throw new IllegalStateException("Compiler not available");
         }
-    };
+    var diagnostics = new DiagnosticCollector<>();var fileManager = new MemoryFileManager(compiler.getStandardFileManager(null, null, null), loader);
 
-    var compilationUnits = List.of(file);
-    var task = compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
+        var file = new SimpleJavaFileObject(
+                URI.create("string:///" + className.replace('.', '/') + JavaFileObject.Kind.SOURCE.extension),
+                JavaFileObject.Kind.SOURCE) {
+            @Override
+            public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+                return sourceCode;
+            }
+        };
 
-    var success = task.call();
-    return new CompilerResult(success, diagnostics, loader);
-  }
+        var compilationUnits = List.of(file);
+        var task = compiler.getTask(null, null, diagnostics, null, null, compilationUnits);
 
-  private static List<Diagnostic> compilationResultHandler(CompilerResult compilerResult){
-    var result = new ArrayList<Diagnostic>();
-    if (!compilerResult.success) {
-      for (var diagnostic : compilerResult.diagnostics.getDiagnostics()) {
-        result.add(new Diagnostic(diagnostic.getLineNumber(), diagnostic.getColumnNumber(), diagnostic.getMessage(Locale.FRANCE)));
-      }
+        var success = task.call();
+        return new CompilerResult(success, diagnostics);
     }
-    return result;
-  }
+
+    private static List<Diagnostic> compilationResultHandler(CompilerResult compilerResult){
+        var result = new ArrayList<Diagnostic>();
+        if (!compilerResult.success) {
+            for (var diagnostic : compilerResult.diagnostics.getDiagnostics()) {
+                result.add(new Diagnostic(
+                        diagnostic.getLineNumber(),
+                        diagnostic.getColumnNumber(),
+                        diagnostic.getMessage(Locale.FRANCE)));
+            }
+        }
+        return result;
+    }
 }
